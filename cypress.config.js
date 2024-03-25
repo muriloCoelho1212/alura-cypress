a51e73c8e9fbfe8d43be4605e645e4cd68583cc0
Murilo <m.coelhos1212@gmail.com>
2024-03-25 11:07:36 -0300

    fix: resolve cypress conflicts in cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+const { defineConfig } = require("cypress")
+
+module.exports = defineConfig({
+  projectId: 'o8sxvk',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      module.exports = defineConfig({
+        projectId: "o8sxvk",
+        e2e: {
+          setupNodeEvents(on, config) {
+            { "reporter"; "mochawesome",
+              "reporterOptions"; 
+                { "reportDir"; "cypress/report/mochawesome-report",
+                  "overwrite"; true,
+                  "html"; true,
+                  "json"; true,
+                  "code"; true,
+                  "timestamp"; "mmddyyyy_HHMMss" }}
+          }
+        }
+      })
     },
   },
-};
+});
