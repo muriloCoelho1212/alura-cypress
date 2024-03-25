20186871100e11f9011ad9d97e2377b6d9acf0ad
Murilo <m.coelhos1212@gmail.com>
2024-03-25 11:47:16 -0300

    feat: create new test to api and use cypress env

--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -1,12 +1,21 @@
 describe('Login Page', () => {
     beforeEach(() => {
         cy.visit('http://localhost:4200/#/home')
+
+        cy.intercept('POST', 'http://localhost:3000/user/login', {
+            statusCode: 401
+        }).as('stubPost')
     })
+    
     it('login user', () => {
       cy.login('murilocoelho', 'teste@123')
     })
-    it('error when login user', () => {  
+    it('error when some field is empty', () => {  
       cy.contains('User name is required!').should('be.visible')
       cy.contains('Password is required!').should('be.visible')
+    })
+    it('unauthorized when login user is invalid', () => {  
+      cy.login('teste', '12345678')
+      cy.wait('@stubPost')
     })
   })
--- cypress/e2e/registerPage.cy.js
+++ cypress/e2e/registerPage.cy.js
@@ -2,14 +2,14 @@ describe('Register Page', () => {
   it('register new user', () => {
     cy.visit('http://localhost:4200/#/home')
 
-    cy.contains('a', 'Register now').click()
+    cy.contains('[data-test="register"]').click()
 
     cy.get('[data-test="email"]').type('murilocoelho@gmail.com')
     cy.get('[data-test="fullName"]').type('Murilo Coelho')
     cy.get('[data-test="registerUserName"]').type('murilocoelho')
     cy.get('[data-test="registerPassword"]').type('teste@123')
 
-    cy.contains('button', 'Register').click()
+    cy.contains('[data-test="btnRegister"]').click()
   })
 
   it('error when register new user', () => {
